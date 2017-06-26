b26e59e96524172a557bd5f9df8796caf3758155
manojkumar1995 <manoj251995kumar@gmail.com>
2017-06-26 12:40:32 +0530

    Updated scripts

--- Remote_Files.sh
+++ Remote_Files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-USERNAME="kishore"
-HOSTS="192.168.0.105"
+USERNAME="rihu"
+HOSTS="192.168.0.107"
 SCRIPT="pwd; find ./ | sed -e 's/[^-][^\/]*\//--/g;s/--/ |-/'"
 for HOSTS in ${HOSTS} ; do
 echo REMOTE HOST:$HOSTS 
--- SearchFile.sh
+++ SearchFile.sh
@@ -1,7 +1,8 @@
 #!bin/sh
 echo "Enter the file extension "
 read filename
-echo "Enter the word"  
+echo "Enter the word to be searched"  
 read word
 find -name "$filename" | xargs grep -i $word
+
 
