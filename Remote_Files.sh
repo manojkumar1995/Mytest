b9f66a49e7b76045e731e1628a55238c6b512a8d
manoj kumar <manoj251995kumar@gmail.com>
2017-06-28 17:54:11 +0530

    Delete Remote_Files.sh

--- Remote_Files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-USERNAME="rihu"
-HOSTS="192.168.0.107"
-SCRIPT="pwd; find ./ | sed -e 's/[^-][^\/]*\//--/g;s/--/ |-/'"
-for HOSTS in ${HOSTS} ; do
-echo REMOTE HOST:$HOSTS 
-echo $HOSTS Files are:::::
-  ssh -l "${USERNAME}" "${HOSTS}" "${SCRIPT}"
-done
